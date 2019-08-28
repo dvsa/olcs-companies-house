556f61226bfb3d1e260a48ca9e62337b0fd36bfb
James Hodgson <james.hodgson@dvsa.gov.uk>
2019-08-28 11:53:30 +0100

    Initial commit

---
+++ src/Service/Exception/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dvsa\Olcs\CompaniesHouse\Service\Exception;
+
+use Dvsa\Olcs\CompaniesHouse\Service\Exception as ServiceException;
+
+/**
+ * NotFound Exception
+ *
+ * @author Dan Eggleston <dan@stolenegg.com>
+ */
+class NotFoundException extends ServiceException
+{
+}
