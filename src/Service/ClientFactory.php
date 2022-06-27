bf5cf0c0b15160b4c36f881bca1442666f097046
James Wragg <james.wragg@dvsa.gov.uk>
2022-06-27 11:12:56 +0100

    VOL-3029: Compatibility with Laminas 3 & general cleanup/refactor.

--- src/Service/ClientFactory.php
+++ src/Service/ClientFactory.php
@@ -3,46 +3,31 @@
 namespace Dvsa\Olcs\CompaniesHouse\Service;
 
 use Dvsa\Olcs\Utils\Client\ClientAdapterLoggingWrapper;
+use Interop\Container\ContainerInterface;
 use RuntimeException;
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
 use Laminas\Http\Client as HttpClient;
 use Olcs\Logging\Log\Logger;
 
-/**
- * Class ClientFactory
- *
- * @author Dan Eggleston <dan@stolenegg.com>
- */
 class ClientFactory implements FactoryInterface
 {
-    /**
-     * @var string
-     */
-    protected $optionKeyName = 'companies_house';
+    const CONFIG_NAMESPACE = 'companies_house';
 
     /**
      * @var array
      */
     protected $options;
 
-    /**
-     * Create service
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     *
-     * @return \Dvsa\Olcs\CompaniesHouse\Service\Client
-     * @throws \RuntimeException
-     */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, string $requestedName, array $options = null): Client
     {
         $client = new Client();
 
-        $httpOptions = $this->getOptions($serviceLocator, 'http');
+        $httpOptions = $this->getOptions($container, 'http');
         $httpClient = new HttpClient();
         $httpClient->setOptions($httpOptions);
 
-        $authOptions = $this->getOptions($serviceLocator, 'auth');
+        $authOptions = $this->getOptions($container, 'auth');
         if (isset($authOptions['username']) && isset($authOptions['password'])) {
             $httpClient->setAuth($authOptions['username'], $authOptions['password']);
         }
@@ -52,9 +37,9 @@ class ClientFactory implements FactoryInterface
 
         $client->setHttpClient($httpClient);
 
-        $clientOptions = $this->getOptions($serviceLocator, 'client');
-        if (!isset($clientOptions['baseuri']) || empty($clientOptions['baseuri'])) {
-            throw new RuntimeException(sprintf('Missing required option %s.client.baseuri', $this->optionKeyName));
+        $clientOptions = $this->getOptions($container, 'client');
+        if (empty($clientOptions['baseuri'])) {
+            throw new RuntimeException(sprintf('Missing required option %s.client.baseuri', static::CONFIG_NAMESPACE));
         }
         $client->setBaseUri($clientOptions['baseuri']);
 
@@ -62,26 +47,29 @@ class ClientFactory implements FactoryInterface
     }
 
     /**
-     * Gets options from configuration based on name.
-     *
-     * @param ServiceLocatorInterface $sl
-     * @param string $key
-     *
-     * @throws \RuntimeException
-     * @return \Laminas\Stdlib\AbstractOptions
+     * @deprecated Not needed after upgrade to Laminas 3
+     * @todo Remove as part of OLCS-28149
      */
-    public function getOptions(ServiceLocatorInterface $sl, $key)
+    public function createService(ServiceLocatorInterface $serviceLocator): Client
+    {
+        return $this($serviceLocator, Client::class);
+    }
+
+    /**
+     * Gets options from configuration based on name.
+     */
+    public function getOptions(ServiceLocatorInterface $sl, string $key): array
     {
         if (is_null($this->options)) {
             $options = $sl->get('Configuration');
-            $this->options = isset($options[$this->optionKeyName]) ? $options[$this->optionKeyName] : array();
+            $this->options = $options[static::CONFIG_NAMESPACE] ?? [];
         }
 
-        $options = isset($this->options[$key]) ? $this->options[$key] : null;
+        $options = $this->options[$key] ?? null;
 
-        if (null === $options) {
+        if (is_null($options)) {
             throw new RuntimeException(
-                sprintf('Options could not be found in "%s.%s".', $this->optionKeyName, $key)
+                sprintf('Options could not be found in "%s.%s".', static::CONFIG_NAMESPACE, $key)
             );
         }
 
